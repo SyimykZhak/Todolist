5eb7374213aa61ad95976cb43888b60e99ff2940
Syimyk Zhak <zhakshybaevv@gmail.com>
2021-01-29 01:04:25 +0300

    added todo closing final

--- main/views.py
+++ main/views.py
@@ -35,3 +35,9 @@ def unmark_todo(request, id):
     todo.is_favorite = False
     todo.save()
     return redirect(test)
+
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
